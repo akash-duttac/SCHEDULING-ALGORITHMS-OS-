b3ebe7ce77b38986b73c0355396925393803429b
akash-duttac <105050803+akash-duttac@users.noreply.github.com>
2023-04-24 23:17:20 +0530

    Update shortest_job_first.c

--- shortest_job_first.c
+++ shortest_job_first.c
@@ -1,52 +1,47 @@
 #include <stdio.h>
-int main(){
-    int p[10]={0}, bt[10]={0}, wt[10]={0}, tat[10]={0}, ct[10]={0};
-    int n, pos, temp, totalTAT=0, totalWT=0, i, j;
+#include <stdlib.h>
+struct process{
+    int pid, bt, ct, tat, wt;
+};
+int main()
+{
+    int n;
     printf("Enter total no. of processes: ");
     scanf("%d", &n);
-    printf("Enter the burst times\n");
-    for (i=0; i<n; i++){
-        printf("P%d: ", i+1);
-        scanf("%d", &bt[i]);
-        p[i] = i+1;
+    struct process arr[n];
+    printf("Enter burst time of each process\n");
+    for (int i=0; i<n; i++){
+        struct process *ptr = arr + i;
+        ptr->pid = i+1;
+        printf("Process %d: ", ptr->pid);
+        scanf("%d", &ptr->bt);
     }
-    //sorting the burst timed
-    for (i=0; i<n; i++){
-        pos = i;
-        for (j=i+1; j<n; j++)
-            if (bt[j] < bt[pos])
-                pos = j;
-
-        temp = bt[i];
-        bt[i] = bt[pos];
-        bt[pos] = temp;
-
-        temp = p[i];
-        p[i] = p[pos];
-        p[pos] = temp;
-    }
-    wt[0] = 0;
-    //calculating the waiting time
-    for (i=1; i<n; i++){
-        wt[i] = 0;
-        for (j=0; j<i; j++)
-            wt[i] += bt[j]; //each wt is added bt of all completed processes
-        totalWT += wt[i];
-    }
-    //completion time
-    for (i = 0; i < n; i++) {
-        if (i == 0) {
-            ct[i] = bt[i];
-        } else {
-            ct[i] = ct[i-1] + bt[i];
+    //sorting the processes by the burst times
+    for (int i=0; i<n; i++){
+        for (int j=i+1; j<n; j++){
+            if (arr[i].bt > arr[j].bt){
+                struct process temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
     }
-    printf("\nP#\tBT\tWT\tTAT\tCT\n");
-    for (i=0; i<n; i++){
-        tat[i] = bt[i] + wt[i]; //each tat
-        totalTAT += tat[i];
-        printf("%d\t%d\t%d\t%d\t%d\n", p[i],bt[i],wt[i],tat[i],ct[i]);
+    //shortest job first algo
+    int current_time = 0;
+    for (int i=0; i<n; i++){
+        struct process *ptr = arr+i;
+        current_time += ptr->bt;
+        ptr->ct = current_time;
     }
-    printf("Avg tat = %.2f", (float)totalTAT/n);
-    printf("\nAvg wt = %.2f", (float)totalWT/n);
+    for (int i=0; i<n; i++){
+        struct process *ptr = arr + i;
+        ptr->tat = ptr->ct;
+        ptr->wt = ptr->tat - ptr->bt;
+    }
+    printf("\nP#\tBT\tCT\tTAT\tWT\n");
+    for (int i=0; i<n; i++){
+        struct process *ptr = arr+i;
+        printf("%d\t%d\t%d\t%d\t%d\n", ptr->pid, ptr->bt, ptr->ct, ptr->tat, ptr->wt);
+    }
+    return 0;
 }
