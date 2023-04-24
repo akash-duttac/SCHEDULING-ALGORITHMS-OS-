932b813cafb50e7fc7ce281b4ae8cf5da0c5140b
akash-duttac <105050803+akash-duttac@users.noreply.github.com>
2023-04-24 23:16:32 +0530

    Update first_come_first_serve.c

--- first_come_first_serve.c
+++ first_come_first_serve.c
@@ -1,36 +1,59 @@
-// C program to perform CPU Scheduling Algorithm
+// C program to implement First Come First Serve CPU Scheduling Algorithm
 #include <stdio.h>
+#include <stdlib.h>
+struct process{
+    int pid, at, bt, ct, tat, wt;
+};
 int main()
 {
-    int at[10] = {0}, bt[10] = {0}, tat[10] = {0}, wt[10] = {0}, ct[10] = {0};
-    int n, sum = 0, totalTAT = 0, totalWT = 0;
-    printf("Enter no. of processes: ");
+    int n;
+    printf("Enter total no. of processes: ");
     scanf("%d", &n);
-    printf("Enter Arrival & Burst Time for each process: \n");
-    for (int i = 0; i < n; i++)
-    {
-        printf("Process %d: ", i + 1);
-        scanf("%d %d", &at[i], &bt[i]);
+    struct process arr[n];
+    int total_time=0;
+    printf("Enter arrival time and burst time of each process\n");
+    for (int i=0; i<n; i++){
+        struct process *ptr = arr + i;
+        ptr->pid = i+1;
+        printf("Process %d: ", ptr->pid);
+        scanf("%d %d", &ptr->at, &ptr->bt);
+        total_time += ptr->bt;
     }
-    for (int i = 0; i < n; i++)
-    {
-        // completion time
-        sum += bt[i];
-        ct[i] += sum;
+    //sort the processes based on arrival times
+    for (int i=0; i<n; i++){
+        for (int j=i+1; j<n; j++){
+            if (arr[i].at > arr[j].at){
+                struct process temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
     }
-    for (int i = 0; i < n; i++)
-    {
-        // turaround time
-        tat[i] = ct[i] - at[i];
-        totalTAT += tat[i];
-        // waiting time
-        wt[i] = tat[i] - bt[i];
-        totalWT += wt[i];
+    // perform fcfs
+    int current_time = 0;
+    for (int i=0; i<n; i++){
+        struct process *ptr = arr+i;
+        if (ptr->at > current_time && current_time==0)
+            current_time += ptr->at;
+        else if (ptr->at > current_time) 
+            current_time += current_time - ptr->at;
+        current_time += ptr->bt;
+        ptr->ct = current_time;
     }
-    printf("\n");
-    printf("P#\tAT\tBT\tCT\tTAT\tWT\n");
-    for (int i = 0; i < n; i++)
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], ct[i], tat[i], wt[i]);
-    printf("\nAverage TAT = %.2f\nAverage WT = %.2f", (float)totalTAT / n, (float)totalWT / n);
+    float avgWT=0, avgTAT=0;
+    for (int i=0; i<n; i++){
+        struct process *ptr = arr + i;
+        ptr->tat = ptr->ct - ptr->at;
+        ptr->wt = ptr->tat - ptr->bt;
+        avgTAT += ptr->tat;
+        avgWT += ptr->wt;
+    }
+    printf("\nP#\tAT\tBT\tCT\tTAT\tWT\n");
+    for (int i=0; i<n; i++){
+        struct process *ptr = arr+i;
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n", ptr->pid, ptr->at, ptr->bt, ptr->ct, ptr->tat, ptr->wt);
+    }
+    printf("\nAverage Waiting Time = %.2f", avgWT/n);
+    printf("\nAverage Turnaroung Time = %.2f", avgTAT/n);
     return 0;
 }
